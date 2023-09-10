766064978dc161ffef5120d417c81bc08e968503
gabrielzinx <gabriel.soaressc58@gmail.com>
2023-09-10 18:39:51 -0400

    style: created module.css

--- capputeeno/src/app/product/[id]/page.tsx
+++ capputeeno/src/app/product/[id]/page.tsx
@@ -1,38 +1,18 @@
 import ButtonBack from "@/components/Buttons/Back";
 import Header from "@/components/Header";
 import ProductInfo from "@/components/ProductInfo";
+import styles from './styles.module.css';
 
 export default function ProductPage({ params }: { params: { id: string } }) {
     return (
-        <main style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            width: '100vw',
-            height: '100%',
-        }}>
+        <main className={styles.container}>
             <Header />
-            <div style={{
-                display: "flex",
-                flexDirection: 'column',
-                alignItems: 'center',
-                width: '100vw',
-                height: 'calc(100vh - 92px)',
-                backgroundColor: 'var(--shapes-light-92-gray)'
-            }}>
-                <nav style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'flex-start',
-                    alignItems: 'center',
-                    width: '84%',
-                    height: '60px',
-                    maxWidth: '1120px'
-                }}>
+            <div className={styles.content}>
+                <nav className={styles.navbar}>
                     <ButtonBack />
                 </nav>
                 <ProductInfo id={params.id} />
             </div>
-        </main >
+        </main>
     )
 }
