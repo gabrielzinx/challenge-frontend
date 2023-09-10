732521fd5b910cdd5bafb85d2410e732b501c23f
gabrielzinx <gabriel.soaressc58@gmail.com>
2023-09-10 18:54:04 -0400

    style: adding limiter in Header

--- capputeeno/src/components/Header/index.tsx
+++ capputeeno/src/components/Header/index.tsx
@@ -6,14 +6,15 @@ import SearchBar from "./SearchBar";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div className={styles.logo}>
-                <Link href={"/"} className={styles.title}>Capputeeno</Link>
-            </div>
-            <div className={styles.actions}>
-                <SearchBar placeholder="Procurando por algo específico?" />
-                <ButtonShoppingBag />
+            <div className={styles.limiter}>
+                <div className={styles.logo}>
+                    <Link href={"/"} className={styles.title}>Capputeeno</Link>
+                </div>
+                <div className={styles.actions}>
+                    <SearchBar placeholder="Procurando por algo específico?" />
+                    <ButtonShoppingBag />
+                </div>
             </div>
         </header>
-
     )
 }
